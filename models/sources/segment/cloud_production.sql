0fa0e3062b8e71a55fe8edbe7f028dfb6308ad63
Anna Glander <anna@materialize.com>
2022-06-22 23:15:52 -0400

    source definitions

---
+++ models/sources/segment/cloud_production.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='source') }}
+--- note: replace UPSTASH_BROKER with your upstash broker url!
+--- note: if developing locally, replace username and password with strings provided w/in upstash
+CREATE SOURCE IF NOT EXISTS {{ this }}
+  FROM KAFKA BROKER 'UPSTASH_BROKER' TOPIC 'cloud_production'
+  WITH (
+      security_protocol = 'SASL_SSL',
+      sasl_mechanisms = 'SCRAM-SHA-256',
+      sasl_username = SECRET segment_upstash_user,
+      sasl_password = SECRET segment_upstash_pw
+  )
+FORMAT BYTES;
